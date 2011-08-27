228f4b2f8d4870c32754313c763a8b2380584285
Tiago César Katcipis <tiagokatcipis@gmail.com>
2011-08-27 12:56:02 -0300

    full udp multicast :D

--- ruby-sockets/cancela.rb
+++ ruby-sockets/cancela.rb
@@ -3,7 +3,7 @@ require 'socket'
 SERVER_IP = 'localhost'
 SERVER_PORT = 3000
 
-MULTICAST_ADDRESS = 'localhost'
+MULTICAST_ADDRESS = '224.0.1.0'
 MULTICAST_PORT    = 7777
 
 MAX_UDP_PACKET_SIZE = 65536
@@ -15,5 +15,7 @@ server_connection = TCPSocket.open(SERVER_IP, SERVER_PORT)  # Connect to server
 
 puts 'Connected to the server, lets do some multicasting at: ' + MULTICAST_ADDRESS
 
-multicast_sender = UDPSocket.new
-multicast_sender.send('ping', 0, MULTICAST_ADDRESS, MULTICAST_PORT) 
+multicast_sender = UDPSocket.open
+multicast_sender.setsockopt(Socket::IPPROTO_IP, Socket::IP_TTL, [1].pack('i'))
+multicast_sender.send('ping', 0, MULTICAST_ADDRESS, MULTICAST_PORT)
+
