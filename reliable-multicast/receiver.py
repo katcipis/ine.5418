8cd17ee4aed58d70d6d5304dd82e536035cf691d
Tiago Katcipis <tiagokatcipis@gmail.com>
2011-11-22 22:24:38 -0200

    horse

--- reliable-multicast/receiver.py
+++ reliable-multicast/receiver.py
@@ -1,3 +1,4 @@
+import socket
 
 _BASE_PORT = 7777
 _IP_ADDR   = '127.0.0.1'
@@ -27,11 +28,21 @@ def get_available_port (group):
     participants.write(_IP_ADDR + _PARTICIPANT_SPLIT_STR + str(port) + '\n')
     participants.close()
     return port
+
         
+def wait_for_start (sock):
+    conn, addr = sock.accept()
+    data = conn.recv(1024) #whatever, just a start ping
+    conn.close()
 
 
 if __name__ == '__main__':
 
     group = get_group()
     port = get_available_port(group)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((_IP_ADDR, port))
+    sock.listen(1)
+
     print('receiver process accepting connections at {ip}:{port}'.format(ip = _IP_ADDR, port=port))
